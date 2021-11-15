87b5dae91690821e934c9dcf9888f0441ff03def
emauel pora <epora@lyracons.com>
2021-11-15 15:34:42 -0300

    Actualización Create

--- src/Traits/Create.php
+++ src/Traits/Create.php
@@ -40,15 +40,34 @@ trait Create
 
             $functions = data_get($this->config, 'functions');
 
+            //Listado de funciones
             foreach($functions as $function => $attributes) {
                 //Enviar todo el array o un elemento iterativo 
                 $limit = 1;
                 $loopData = &$entityData;
 
-                //Verificar que la función se llame mas de una vez
-                if(isset($attributes['loop_call'])) {
-                    $limit = count($entityData[$attributes['loop_call']]);
-                    $loopData = &$entityData[$attributes['loop_call']];
+                //Loop de una función específica.
+                if(isset($attributes['loop'])) {
+                    foreach($attributes['loop'] as $keyLoop => $valueLoop) {
+                        switch($keyLoop) {
+                            case 'call':
+                                $limit = count($entityData[$valueLoop]);
+                                $loopData = &$entityData[$valueLoop];
+                                break;
+
+                            case 'key':
+                            case 'unique_key':
+                                $loopList = array_column($entityData[$attributes['loop']['call']], $valueLoop);
+                                if($keyLoop == 'unique_key') {
+                                    $loopList = array_unique($loopList);
+                                    $limit = count($loopList);
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
                 }
 
                 $requestParametersExist = false;
@@ -65,6 +84,15 @@ trait Create
                             $requestData[$param] = $loopData[$i][$value];
                         }
                     }
+
+                    //Agregado de datos de loop
+                    $requestData['loop_index'] = $i;
+                    $requestData['loop_total'] = $limit;
+                    if(isset($loopList)) {
+                        unset($requestData['loop_list']);
+                        $requestData['loop_list'] = $loopList;
+                    }
+
 
                     $entity = $this->callEntity($function, $requestData);
 
